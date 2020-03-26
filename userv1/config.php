f0cfeea7d39cfe36e9c13797f1c6b2ca04b083d3
nandabagusprasetya <62687762+nandabagusprasetya@users.noreply.github.com>
2020-03-26 13:50:32 +0700

    Add files via upload

---
+++ userv1/config.php
@@ -0,0 +1,9 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<admin','mikhmon>|>mZWfoZ9ybGRkaQ==');
+
+
+
+
+
+$data['RB941'] = array ('1'=>'RB941!192.168.1.1','RB941@|@admin','RB941#|#mZWfoZ9ybGRkaQ==','RB941%mudakreatif.net','RB941^mudakreatif.net','RB941&Rp','RB941*10','RB941(1','RB941)','RB941=10','RB941@!@enable');
